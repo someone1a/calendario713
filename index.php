5466c3cde2538861db7c5476e2828a37ae43a452
Walter Carrasco <waltercarrasco440@gmail.com>
2023-12-04 10:19:15 -0300

    Refactor CSS styles and update JavaScript functions

--- index.php
+++ index.php
@@ -32,66 +32,88 @@
         </footer>
     </footer>
     <script>
-    function isToday(date) {
-    const today = new Date();
-    return (
-        date.getDate() === today.getDate() &&
-        date.getMonth() === today.getMonth() &&
-        date.getFullYear() === today.getFullYear()
-    );
-}
-
-fetch('php/jsonGenerateCalendar.php')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+        function isToday(date) {
+            const today = new Date();
+            return (
+                date.getDate() === today.getDate() &&
+                date.getMonth() === today.getMonth() &&
+                date.getFullYear() === today.getFullYear()
+            );
         }
-        return response.json();
-    })
-    .then(data => {
-        const esLocale = 'es-ES';
-        const dateFormatter = new Intl.DateTimeFormat(esLocale, { day: 'numeric', month: 'long', year: 'numeric' });
 
-        data.forEach(event => {
-            const eventDate = new Date(event.Fecha + 'T00:00:00');
-            const eventName = event.Titulo;
-            const eventType = event.tipo;
-            const matchingCells = document.querySelectorAll(`td[data-date="${event.Fecha}"]`);
-            
-            matchingCells.forEach(cell => {
-                cell.classList.add(eventType);
-                const table = cell.closest('table');
-                const divSiguiente = table.nextElementSibling;
-                if (divSiguiente && divSiguiente.tagName === 'DIV') {
-                    const formattedDate = dateFormatter.format(eventDate);
-                    divSiguiente.innerHTML += `<p><i class="fas fa-bookmark"></i> ${formattedDate} - ${eventName} (${eventType})</p>`;
-                } else {
-                    console.log('No se encontró un div siguiente a la tabla.');
+        fetch('php/jsonGenerateCalendar.php')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                const esLocale = 'es-ES';
+                const dateFormatter = new Intl.DateTimeFormat(esLocale, {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                });
+
+                data.forEach(event => {
+                    const eventDate = new Date(event.Fecha + 'T00:00:00');
+                    const eventName = event.Titulo;
+                    const eventType = event.tipo;
+                    const matchingCells = document.querySelectorAll(`td[data-date="${event.Fecha}"]`);
+
+                    matchingCells.forEach(cell => {
+                        cell.classList.add(eventType);
+                        const table = cell.closest('table');
+                        const divSiguiente = table.nextElementSibling;
+                        if (divSiguiente && divSiguiente.tagName === 'DIV') {
+                            const formattedDate = dateFormatter.format(eventDate);
+                            divSiguiente.innerHTML += `<p><i class="fas fa-bookmark"></i> ${formattedDate} - ${eventName} (${eventType})</p>`;
+                        } else {
+                            console.log('No se encontró un div siguiente a la tabla.');
+                        }
+                    });
+                });
+            })
+            .catch(e => {
+                console.log(e.message);
+            });
+
+        function addTodayClassToCells() {
+            const cells = document.querySelectorAll('td[data-date]');
+            const today = new Date();
+            console.log('Today:', today);
+
+            cells.forEach(cell => {
+                const cellDateAttribute = cell.getAttribute('data-date');
+                console.log('Cell Date Attribute:', cellDateAttribute);
+
+
+                const cellDate = new Date(cellDateAttribute);
+                if (today.toDateString() === cellDate.toDateString()) {
+                    cell.classList.add('today');
                 }
             });
-        });
-    })
-    .catch(e => {
-        console.log(e.message);
-    });
-    function addTodayClassToCells() {
-    const cells = document.querySelectorAll('td[data-date]');
-    const today = new Date();
-    console.log('Today:', today);
-
-    cells.forEach(cell => {
-        const cellDateAttribute = cell.getAttribute('data-date');
-        console.log('Cell Date Attribute:', cellDateAttribute);
-
-
-        const cellDate = new Date(cellDateAttribute);
-        if (today.toDateString() === cellDate.toDateString()) {
-            cell.classList.add('today');
         }
-    });
-}
-document.addEventListener('DOMContentLoaded', addTodayClassToCells);
+        const dateFormatter = new Intl.DateTimeFormat('es-ES', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
 
+        const addDate = () => {
+            const table = document.querySelector('table');
+            const divSiguiente = table.nextElementSibling;
+            if (divSiguiente && divSiguiente.tagName === 'DIV') {
+                const formattedDate = dateFormatter.format(new Date());
+                divSiguiente.innerHTML += `<p class="txcenter"><i class="fa-solid fa-calendar" style="color: #50e259;"></i> Hoy Es: ${formattedDate}</p>`;
+            } else {
+                console.log('No se encontró un div siguiente a la tabla.');
+            }
+        };
+
+        document.addEventListener('DOMContentLoaded', addDate);
+        document.addEventListener('DOMContentLoaded', addTodayClassToCells);
     </script>
 </body>
 
